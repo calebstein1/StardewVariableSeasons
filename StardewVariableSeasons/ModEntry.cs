de9b68b34a8a4765e45ce8a62687d8bf242c3c50
Caleb Stein <caleb@calebstein.net>
2024-01-08 10:28:58 -0800

    randomly pull date of season change and write to save file

--- StardewVariableSeasons/ModEntry.cs
+++ StardewVariableSeasons/ModEntry.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
@@ -9,26 +10,60 @@ namespace StardewVariableSeasons
 {
     internal sealed class ModEntry : Mod
     {
+        private static int GenNextChangeDate()
+        {
+            var rnd = new Random();
+            var rndNum = rnd.Next(100);
+
+            var map = new List<(int, int)>
+            {
+                (0, 23),
+                (2, 24),
+                (7, 25),
+                (12, 26),
+                (22, 27),
+                (37, 28),
+                (52, 2),
+                (62, 3),
+                (67, 4),
+                (72, 5),
+                (74, 6),
+                (75, 7)
+            };
+
+            foreach (var (upper, result) in map)
+            {
+                if (rndNum <= upper)
+                    return result;
+            }
+
+            return 1;
+        }
+        
         public override void Entry(IModHelper helper)
         {
             helper.Events.GameLoop.DayEnding += OnDayEnding;
+            helper.Events.GameLoop.DayStarted += OnDayStarted;
         }
 
         private void OnDayEnding(object sender, DayEndingEventArgs e)
         {
-            if (!Context.IsWorldReady)
-                return;
-            
-            Monitor.Log($"{Helper.Data.ReadSaveData<ModData>("next-season-change").NextSeasonChange}", LogLevel.Debug);
-
             if (Game1.Date.DayOfMonth == 14)
             {
                 var nextSeasonChange = new ModData
                 {
-                    NextSeasonChange = 24
+                    NextSeasonChange = GenNextChangeDate()
                 };
                 
                 Helper.Data.WriteSaveData("next-season-change", nextSeasonChange);
+            }
+        }
+
+        private void OnDayStarted(object sender, DayStartedEventArgs e)
+        {
+            if (Game1.Date.DayOfMonth == Helper.Data.ReadSaveData<ModData>("next-season-change").NextSeasonChange)
+            {
+                Monitor.Log("Change to next season", LogLevel.Debug);
             }
         }
     }
