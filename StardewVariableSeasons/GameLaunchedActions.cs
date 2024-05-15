2470881f3d1f7bb2693cb823caff68440457c9ec
Caleb Stein <caleb@calebstein.net>
2024-05-15 16:20:32 -0700

    fix bugs with settings panel

--- StardewVariableSeasons/GameLaunchedActions.cs
+++ StardewVariableSeasons/GameLaunchedActions.cs
@@ -11,56 +11,58 @@ namespace StardewVariableSeasons
     }
     public static class GameLaunchedActions
     {
-        private static void UpdateState(ModConfig config)
+        private static void UpdateState(ModConfig config, IModHelper helper)
         {
             Game1.season = config.Season;
             ModEntry.SeasonByDay = config.SeasonByDay;
+            Game1.setGraphicsForSeason();
+            Game1.netWorldState.Value.UpdateFromGame1();
+            
+            var seasonByDay = new ModData
+            {
+                SeasonByDay = ModEntry.SeasonByDay
+            };
+                
+            helper.Data.WriteSaveData("season-by-day", seasonByDay);
         }
         public static void OnGameLaunched(IMonitor monitor, IModHelper helper, IManifest manifest, object sender, GameLaunchedEventArgs e)
         {
             var configMenu = helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
             if (configMenu is null) return;
 
-            var Config = new ModConfig
+            var config = new ModConfig
             {
                 Season = Game1.season,
                 SeasonByDay = ModEntry.SeasonByDay
             };
             configMenu.Register(
                 mod: manifest,
-                reset: () => Config = new ModConfig
+                reset: () => config = new ModConfig
                 {
                     Season = Game1.season,
                     SeasonByDay = ModEntry.SeasonByDay
                 },
-                save: () => UpdateState(Config)
+                save: () => UpdateState(config, helper)
             );
             
+            configMenu.AddParagraph(
+                mod: manifest,
+                text: () => "These options can be used to correct the in-game and calendar seasons should they become incorrect for any reason"
+            );
+
             configMenu.AddTextOption(
                 mod: manifest,
                 name: () => "In-Game Season",
-                getValue: () => Config.Season.ToString(),
-                setValue: value => Config.Season = value switch
-                {
-                    "Spring" => Season.Spring,
-                    "Summer" => Season.Summer,
-                    "Fall" => Season.Fall,
-                    "Winter" => Season.Winter
-                },
+                getValue: () => Game1.season.ToString(),
+                setValue: value => config.Season = SeasonUtils.StrToSeason(value.ToLower()),
                 allowedValues: new[] { "Spring", "Summer", "Fall", "Winter" }
             );
 
             configMenu.AddTextOption(
                 mod: manifest,
                 name: () => "Calendar Season",
-                getValue: () => Config.SeasonByDay.ToString(),
-                setValue: value => Config.SeasonByDay = value switch
-                {
-                    "Spring" => Season.Spring,
-                    "Summer" => Season.Summer,
-                    "Fall" => Season.Fall,
-                    "Winter" => Season.Winter
-                },
+                getValue: () => ModEntry.SeasonByDay.ToString(),
+                setValue: value => config.SeasonByDay = SeasonUtils.StrToSeason(value.ToLower()),
                 allowedValues: new[] { "Spring", "Summer", "Fall", "Winter" }
             );
         }
